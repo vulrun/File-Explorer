39bb5ca5e962600e17fddb05b9401372ee498d79
Web CDN <ironinblood@gmail.com>
2018-01-24 16:18:21 +0530

    Updated Google Analytics Code

--- explorer.php
+++ explorer.php
@@ -533,6 +533,14 @@ if( is_array($errors) ){
 			footer .left-align, footer .right-align {float: none !important; text-align: center !important;}
 		}
 	</style>
+	<script>
+		(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+			(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+			m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+		})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+		ga('create', 'UA-113019966-1', 'auto');
+		ga('send', 'pageview');
+	</script>
 </head>
 <body class="grey lighten-3">
 	<header>
@@ -748,14 +756,6 @@ if( is_array($errors) ){
 	-->
 	<script src="<?php echo $config->assets; ?>/jquery.min.js"></script>
 	<script src="<?php echo $config->assets; ?>/materialize.min.js"></script>
-	<script>
-		(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-			(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-			m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-		})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-		ga('create', 'UA-113019966-1', 'auto');
-		ga('send', 'pageview');
-	</script>
 	<script>
 		(function($){
 			$.fn.clickSort = function() {
